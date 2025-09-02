c06eb08a20f7e8d54df1762876d7757ec2b329ff
David Studholme <d.j.studholme@exeter.ac.uk>
2025-09-02 19:38:25 +0000

    Ran QUAST on assemblies

--- 01_Pectobacterium_misassembly/commands.sh
+++ 01_Pectobacterium_misassembly/commands.sh
@@ -22,6 +22,18 @@ gzip NCPPB929-illumina.1.fq NCPPB929-illumina.2.fq NCPPB929-ont.fq
 ### QC the Illumina data
 trim_galore -q 30 --paired NCPPB929-illumina.1.fq.gz NCPPB929-illumina.2.fq.gz
 
+### Align Illumina reads against the GCA_040224395.1 / ASM4022439v1 genome assembly. Assumes we have installed  BWA version 0.7.17-r1188.
+bwa index GCA_040224395.1_ASM4022439v1_genomic.fna
+bwa mem -t 16 GCA_040224395.1_ASM4022439v1_genomic.fna NCPPB929-illumina.1_val_1.fq.gz NCPPB929-illumina.2_val_2.fq.gz | samtools sort -@8 -o ASM4022439v1.illumina.bam
+samtools index ASM4022439v1.illumina.bam
+
+### Align Illumina reads against the GCA_041228105.1 / NCPPB929v1_ONT genome assembly. Assumes we have installed  BWA version 0.7.17-r1188.
+bwa index GCA_041228105.1_NCPPB929v1_ONT_genomic.fna
+bwa mem -t 16 GCA_041228105.1_NCPPB929v1_ONT_genomic.fna NCPPB929-illumina.1_val_1.fq.gz NCPPB929-illumina.2_val_2.fq.gz | samtools sort -@8 -o NCPPB929v1.illumina.bam
+samtools index NCPPB929v1.illumina.bam
+
+
+
 
 
 
